3fb12f00397419ea295daf8a1062009e1115863c
Jiaxin Lu <122422320+JiaxinLu98@users.noreply.github.com>
2025-02-23 18:20:19 -0600

    Delete HW1/hw01-02-student.sql

--- HW1/hw01-02-student.sql
+++
@@ -1,5 +0,0 @@
-SELECT SUM(Num_Violations)
-FROM Violations
-WHERE Camera_ID IN (SELECT Camera_ID
-                        FROM Cameras
-                        WHERE Intersection = 'ROOSEVELT AND HALSTED' AND Violation_Date LIKE '%2020');
